d2c4ba09e83ce8f4f31407a4b0524cb304f6c19c
Elliot Rudkin <Elliot@letswork.io>
2025-06-23 17:41:43 +0200

    initial commit

--- Tests/DateFormatterUtilitiesTests/DateFormatterUtilitiesTests.swift
+++ Tests/DateFormatterUtilitiesTests/DateFormatterUtilitiesTests.swift
@@ -2,11 +2,33 @@ import XCTest
 @testable import DateFormatterUtilities
 
 final class DateFormatterUtilitiesTests: XCTestCase {
-    func testExample() throws {
-        // XCTest Documentation
-        // https://developer.apple.com/documentation/xctest
 
-        // Defining Test Cases and Test Methods
-        // https://developer.apple.com/documentation/xctest/defining_test_cases_and_test_methods
+    func testShortDateString() {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "dd/MM/yyyy"
+        
+        let date = Date(timeIntervalSince1970: 0)  // 1 Jan 1970
+        XCTAssertEqual(date.shortDateString, formatter.string(from: date))
+    }
+
+    func testISO8601String() {
+        let date = Date(timeIntervalSince1970: 0)
+        let isoString = date.iso8601String
+        XCTAssertTrue(isoString.contains("1970"))
+    }
+    
+    func testDaysBetween() {
+        let calendar = Calendar.current
+        let date1 = Date()
+        let date2 = calendar.date(byAdding: .day, value: -5, to: date1)!
+        XCTAssertEqual(date1.daysBetween(date2), 5)
+    }
+    
+    func testInitFromShortDateString() {
+        let dateString = "23/06/2025"
+        let date = Date(fromShortDateString: dateString)
+        
+        XCTAssertNotNil(date)
+        XCTAssertEqual(date?.shortDateString, dateString)
     }
 }
